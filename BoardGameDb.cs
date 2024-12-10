4eb04b3deeadfbdb2df2ec179d85521eed8a533c
Ben Sadick <ben.sadick.data@gmail.com>
2024-12-09 21:03:53 -0500

    removed old db contexts

--- BoardGameDb.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-class BoardGameDb : DbContext
-{
-    public BoardGameDb(DbContextOptions<BoardGameDb> options)
-        : base(options) { }
-
-    public DbSet<BoardGame> BoardGames => Set<BoardGame>();
-}
